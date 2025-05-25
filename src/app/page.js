77cd6dad439fb64580f628de663236101d8a0836
Bill Huang <bill.huang2001@gmail.com>
2025-05-25 19:54:43 +0800

    gpt-4o: Added NavigationBar component and integrated it into the homepage.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import HeroSection from "../components/HeroSection";
 import NewsBanner from "../components/NewsBanner";
+import NavigationBar from "../components/NavigationBar";
 
 export default function Home() {
   return (
     <div>
       {/* News / Updates Banner */}
       <NewsBanner />
+
+      {/* Navigation Bar */}
+      <NavigationBar />
 
       {/* Hero Section */}
       <HeroSection />
