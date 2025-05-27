c836ccbdabe6be6be0266eb8557a1fba4c43a37c
Bill Huang <bill.huang2001@gmail.com>
2025-05-27 21:21:16 +0800

    gpt-4o: Improved ExampleOutput layout and added optional caption.

--- src/components/ExampleOutput.jsx
+++ src/components/ExampleOutput.jsx
@@ -2,14 +2,19 @@ import React from "react";
 
 const ExampleOutput = ({ src, alt, caption }) => {
   return (
-    <div className="flex justify-center items-center">
-      <div className="relative max-w-xs w-full aspect-square">
+    <div className="flex flex-col justify-center items-center">
+      <div className="relative max-w-sm w-full aspect-square">
         <img
           src={src}
           alt={alt}
           className="rounded-lg shadow-lg w-full h-full object-contain"
         />
       </div>
+      {caption && (
+        <p className="mt-2 text-center text-sm text-gray-700 dark:text-gray-300">
+          {caption}
+        </p>
+      )}
     </div>
   );
 };
